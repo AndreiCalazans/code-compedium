83362970f66f41740873ce6243793e94a20e92d4
Andrei Calazans <andreixoc@hotmail.com>
2023-12-22 14:24:15 -0300

    Structs

--- src/learn_rust/src/main.rs
+++ src/learn_rust/src/main.rs
@@ -1,10 +1,12 @@
 mod basics;
 mod funcs;
 mod ownership;
+mod structs;
 
 fn main() {
     println!("Hello Andrei!");
     basics::the_basic();
     funcs::function_examples();
     ownership::ownership_examples();
+    structs::test_structs();
 }
---
+++ src/learn_rust/src/structs.rs
@@ -0,0 +1,51 @@
+
+pub struct User {
+    username: String,
+    email: String,
+    sign_in_count: u64,
+    active: bool,
+}
+
+pub struct Book {
+    uuid: u32,
+    author: String,
+    title: String,
+    description: String,
+}
+
+pub fn make_user() -> User {
+    User {
+        email: String::from("user@example.com"),
+        username: String::from("user123"),
+        active: true,
+        sign_in_count: 1,
+    }
+}
+
+pub fn make_book() -> Book {
+    Book {
+        uuid: 3242342342,
+        author: String::from("Andrei"),
+        title: String::from("The Book"),
+        description: String::from("The description goes here somwhere"),
+    }
+}
+
+#[derive(Debug)]
+struct Color(i32, i32, i32);
+
+pub fn test_structs() {
+    let user = make_user();
+    println!("User: {}", user.username);
+    let book = make_book();
+    println!("Book: {}", book.title);
+    let user2 = User {
+        email: String::from("user2@email.com"),
+        username: String::from("Other"),
+        ..user
+    };
+    println!("User two: {}", user2.email);
+
+    let black = Color(0, 0, 0);
+    println!("Tuple as color: {:?}", black);
+}
