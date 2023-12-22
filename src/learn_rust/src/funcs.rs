60257ea78a722ca2f5d19871acca0642d73926ed
Andrei Calazans <andreixoc@hotmail.com>
2023-12-22 14:04:37 -0300

    A higher order fun in Rust

--- src/learn_rust/src/funcs.rs
+++ src/learn_rust/src/funcs.rs
@@ -3,6 +3,9 @@ pub fn function_examples() {
     greet();
     add_two_numbers(5, 10);
     println!("multiply {}", multiply(2, 10));
+
+    let result = apply_function(square, 5);
+    println!("Result is: {}", result);
 }
 
 fn greet() {
@@ -17,4 +20,17 @@ fn add_two_numbers(x: i32, y: i32) {
 fn multiply(x: i32, y: i32) -> i32 {
     x * y // no semicolon means this is the return value
 }
+
+// Higher order functions in Rust.
+fn apply_function<F>(f: F, x: i32) -> i32
+where
+    F: Fn(i32) -> i32,
+{
+    f(x)
+}
+
+fn square(x: i32) -> i32 {
+    x * x
+}
+
 
